1fa7c80643e9bdc643dcaa8916758bcde69a6ee5
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-05-09 13:32:28 +0530

    Stable version- with camera and video options

--- test.py
+++ test.py
@@ -1,20 +1,44 @@
 import numpy as np
 import cv2
 import tensorflow as tf
+import easygui
 
+# Load the face detection cascade classifier
 face_detection = cv2.CascadeClassifier('haar_cascade_face_detection.xml')
-camera = cv2.VideoCapture(0)
+
+# Prompt the user to choose between using the camera or uploading a video file
+msg = "Choose an option"
+title = "Facial Expression Recognition"
+choices = ["Camera", "Video File"]
+choice = easygui.buttonbox(msg, title, choices)
+
+# Initialize the camera or read the video file
+if choice == "Camera":
+    camera = cv2.VideoCapture(0)
+elif choice == "Video File":
+    path = easygui.fileopenbox()
+    camera = cv2.VideoCapture(path)
+
+# Set the camera resolution
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1024)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
+
+# Set the face detection settings
 settings = {
     'scaleFactor': 1.3,
     'minNeighbors': 5,
     'minSize': (50, 50)
 }
 
+# Define the facial expression labels
 labels = ['Surprise', 'Normal', 'Angry', 'Happy', 'Sad']
 
+# Load the pre-trained Keras model
 model = tf.keras.models.load_model('network-5Labels.h5')
+
+##customize window size
+# cv2.namedWindow('Facial Expression', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('Facial Expression', 800, 600)
 
 ## start
 
@@ -25,6 +49,8 @@ while True:
     ret, img = camera.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     detected = face_detection.detectMultiScale(gray, **settings)
+
+
 
     for x, y, w, h in detected:
         cv2.rectangle(img, (x, y), (x+w, y+h), (245, 135, 66), 2)
@@ -51,6 +77,10 @@ while True:
     cv2.imshow('Facial Expression', img)
 
     if cv2.waitKey(5) != -1:
+        break
+
+    # Add a button to exit
+    if cv2.getWindowProperty('Facial Expression', cv2.WND_PROP_VISIBLE) < 1:
         break
 
 camera.release()

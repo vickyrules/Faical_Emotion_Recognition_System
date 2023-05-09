d5207a2b8936ae5a0d4040bc8b281f4c56c7eeb8
vickyrules <VICKYRULES1705@GMAIL.COM>
2023-05-09 15:45:48 +0530

    Stable version- with timeline

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -45,10 +47,17 @@ model = tf.keras.models.load_model('network-5Labels.h5')
 total_frames = 0
 correct_predictions = 0
 
+fps = camera.get(cv2.CAP_PROP_FPS) # get frame rate of video
+current_time = 0
+
+
 while True:
     ret, img = camera.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     detected = face_detection.detectMultiScale(gray, **settings)
+
+
+
 
 
 
@@ -63,16 +72,40 @@ while True:
 
         state = labels[predictions]
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img,state,(x+10,y+15), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
+        cv2.putText(img,state,(x+10,y+15), font, 0.5, (255, 255, 255), 2, cv2.LINE_8)
 
         # Calculate accuracy for each frame
         total_frames += 1
         if state == 'Normal':
             correct_predictions += 1
-        accuracy = round(correct_predictions / total_frames, 2)
-
+        accuracy = round(correct_predictions / total_frames, 2) * 100
         # Display accuracy in the video window
-        cv2.putText(img, f'Accuracy: {accuracy*100}%', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        
+        cv2.putText(img, f'Accuracy: {accuracy:.04f}%', (20, 90), cv2.FONT_HERSHEY_COMPLEX, 1, (10, 120, 255), 2, cv2.LINE_AA)
+
+        if choice == "Video File":
+        #timeline
+
+            timeline_position = (img.shape[1] - 100, 30)
+            # Get the current position of the video in seconds
+            current_pos = int(camera.get(cv2.CAP_PROP_POS_MSEC)) // 1000
+            # Get the total duration of the video in seconds
+            total_duration = int(camera.get(cv2.CAP_PROP_FRAME_COUNT)) // int(camera.get(cv2.CAP_PROP_FPS))
+            # Calculate the current position as a percentage of the total duration
+            current_percent = int(current_pos / total_duration * 100)
+            # Draw a yellow bar to indicate the current position on the timeline
+            cv2.rectangle(img, (0, 0), (int(current_percent * 5.12), 10), (50, 220, 255), -1)
+            cv2.putText(img, f' {total_duration//60:02d}:{total_duration%60:02d}', timeline_position, cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (10, 120, 255), 2,cv2.LINE_AA)
+            # # Display the frame along with the timeline
+            # cv2.imshow('Facial Expression', img)
+        #time
+            current_time = round(total_frames / fps, 2)
+            minutes = int(current_time // 60)
+            seconds = int(current_time % 60)
+            cv2.putText(img, f'{minutes:02d}:{seconds:02d}', (20, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (10, 120, 255), 2,
+                        cv2.LINE_AA)
+
+
 
     cv2.imshow('Facial Expression', img)
 
